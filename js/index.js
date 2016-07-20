09028177652497b848a2c4855f66916af75d01a0
harshitvsaxena <harshitsaxena.com@gmail.com>
2016-07-20 19:38:48 +0530

    Structure of page fixed
    Signed-off-by: harshitvsaxena <harshitsaxena.com@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,15 +1,27 @@
+var isClosed = true; // Navbar status
 $(document).ready(function() {
   $("#nav-toggle").click(function() {
     this.classList.toggle( "active" );
-    var testLeft = $(".header").css("margin-left");
-    if(testLeft !== 0) {
+    if(isClosed) {
       $(".header").animate({
         marginLeft: 0
       }, 300);
+      isClosed = false;
     } else {
       $(".header").animate({
         marginLeft: -250
       }, 300);
+      isClosed = true;
     }
   });
+});
+$(window).resize(function() {
+  if(isClosed) {
+    $(".header").animate({
+      marginLeft: 0
+    }, 300);
+    var that = document.getElementById("nav-toggle");
+    that.classList.toggle('active');
+    isClosed = false;
+  }
 });
