37eabb9b90700cc4e9c7c80ad5e3cdaf22c0c5e9
harshitvsaxena <harshitsaxena.com@gmail.com>
2016-07-21 16:07:04 +0530

    All the bugs fixed
    Signed-off-by: harshitvsaxena <harshitsaxena.com@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,27 +1,63 @@
-var isClosed = true; // Navbar status
+var isClosed;
+var navbarLeftSize = $("#left-menu-navbar").width();
 $(document).ready(function() {
+
+  //Fix for all the screen sizes
+  if(screen.width < screen.height) {
+    navbarLeftSize = screen.width * 0.75;
+    $("#left-menu-navbar").css("width", navbarLeftSize);
+    $("#left-menu-navbar").css("margin-left", -navbarLeftSize);
+    $("#navbar-toggle-button").css("display", "block");
+    $(".content").css("padding-left","0px");
+    isClosed = true;
+  } else {
+    //Default
+    navbarLeftSize = 250;
+    $("#left-menu-navbar").css("width", navbarLeftSize);
+    $("#left-menu-navbar").css("margin-left", "0");
+    $("#navbar-toggle-button").css("display", "none");
+    $(".content").css("padding-left",navbarLeftSize);
+    isClosed = false;
+  }
+
   $("#nav-toggle").click(function() {
     this.classList.toggle( "active" );
     if(isClosed) {
-      $(".header").animate({
+      $("#left-menu-navbar").animate({
         marginLeft: 0
       }, 300);
       isClosed = false;
     } else {
-      $(".header").animate({
-        marginLeft: -250
+      $("#left-menu-navbar").animate({
+        marginLeft: -navbarLeftSize
       }, 300);
       isClosed = true;
     }
   });
+
 });
 $(window).resize(function() {
-  if(isClosed) {
-    $(".header").animate({
-      marginLeft: 0
-    }, 300);
-    var that = document.getElementById("nav-toggle");
-    that.classList.toggle('active');
-    isClosed = false;
+  if(screen.width < screen.height) {
+    navbarLeftSize = screen.width * 0.75;
+    $("#left-menu-navbar").css("width", navbarLeftSize);
+    $("#left-menu-navbar").css("margin-left", -navbarLeftSize);
+    $("#navbar-toggle-button").css("display", "block");
+    $(".content").css("padding-left","0px");
+    if(!isClosed) {
+      var that = document.getElementById("nav-toggle");
+      that.classList.toggle( "active" );
+      isClosed = true;
+    }
+  } else {
+    //Default
+    navbarLeftSize = 250;
+    $("#left-menu-navbar").css("width", navbarLeftSize);
+    $("#left-menu-navbar").css("margin-left", "0");
+    $("#navbar-toggle-button").css("display", "none");
+    $(".content").css("padding-left", navbarLeftSize);
+    if(!isClosed) {
+      var that = document.getElementById("nav-toggle");
+      that.classList.toggle( "active" );
+    }
   }
 });
