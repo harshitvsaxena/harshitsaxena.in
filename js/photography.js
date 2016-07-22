464ae10085364964e0a2ac25241cf4999884f191
harshitvsaxena <harshitsaxena.com@gmail.com>
2016-07-22 10:55:35 +0530

    Photography added
    Signed-off-by: harshitvsaxena <harshitsaxena.com@gmail.com>

---
+++ js/photography.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  var numberOfImages = 22;
+  for(var i=1; i<=numberOfImages; i++) {
+    $("#imagesContainer").append(' \
+      <div class="col-md-3 col-sm-4 col-xs-6"> \
+        <img src="../../img/photography/' + i + '.jpg" style="width: 100%; height: 250px;" /> \
+      </div> \
+    ');
+  }
+});
