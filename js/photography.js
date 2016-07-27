55a69718e0ebc66e283b3d04f7e672fbae15a5e2
harshitvsaxena <harshitsaxena.com@gmail.com>
2016-07-27 17:52:58 +0530

    Major changes, 80% done
    Signed-off-by: harshitvsaxena <harshitsaxena.com@gmail.com>

--- js/photography.js
+++ js/photography.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  var numberOfImages = 22;
+  var numberOfImages = 21;
   for(var i=1; i<=numberOfImages; i++) {
     $("#imagesContainer").append(' \
       <div class="col-md-3 col-sm-4 col-xs-6"> \
-        <img src="../../img/photography/' + i + '.jpg" style="width: 100%; height: 250px;" /> \
+        <img src="../img/photography/' + i + '.jpg" style="width: 100%; height: 250px;" /> \
       </div> \
     ');
   }
